e93ebae5d932cea0469713de65ea5cdadda7d03d
Simon Busley <simon.busley@gmail.com>
2023-11-17 22:13:54 +0100

    feat: excluding the root

--- interfaces/ZottelkeeperPluginSettings.ts
+++ interfaces/ZottelkeeperPluginSettings.ts
@@ -1,24 +1,23 @@
-import { SortOrder } from './../models';
-import { IndexItemStyle } from './IndexItemStyle';
+import { SortOrder } from "./../models";
+import { IndexItemStyle } from "./IndexItemStyle";
 
 export interface ZoottelkeeperPluginSettings {
-	indexPrefix: string;
-	indexItemStyle: IndexItemStyle;
-	indexTagValue: string;
-	indexTagBoolean: boolean;
-	indexTagLabel: string;
-	cleanPathBoolean: boolean;
-	indexTagSeparator: string;
-	folderEmoji: string;
-	fileEmoji: string;
-	enableEmojis: boolean;
-	foldersIncluded: string;
-	foldersExcluded: string;
-  	sortOrder: SortOrder;
-	addSquareBrackets: boolean;
-	embedSubIndex: boolean;
-	templateFile: string;
-	frontMatterSeparator: string;
-    [key: string]: any;
-
+  indexPrefix: string;
+  indexItemStyle: IndexItemStyle;
+  indexTagValue: string;
+  indexTagBoolean: boolean;
+  indexTagLabel: string;
+  cleanPathBoolean: boolean;
+  indexTagSeparator: string;
+  folderEmoji: string;
+  fileEmoji: string;
+  enableEmojis: boolean;
+  foldersIncluded: string;
+  foldersExcluded: string;
+  sortOrder: SortOrder;
+  addSquareBrackets: boolean;
+  embedSubIndex: boolean;
+  templateFile: string;
+  frontMatterSeparator: string;
+  [key: string]: any;
 }
